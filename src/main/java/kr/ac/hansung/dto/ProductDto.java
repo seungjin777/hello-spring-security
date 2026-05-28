efeccbeb9acb357ff2e6d894b1abededdea9dc1a
seungjin777 <ds020309@naver.com>
2026-05-28 14:21:19 +0900

    feat: 수정 유효성 검사, 수정 완료 flash 메시지 적용

--- src/main/java/kr/ac/hansung/dto/ProductDto.java
+++ src/main/java/kr/ac/hansung/dto/ProductDto.java
@@ -1,5 +1,9 @@
 package kr.ac.hansung.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +11,15 @@ import lombok.Setter;
 @Setter
 public class ProductDto {
 
+    @NotBlank(message = "상품명 입력은 필수입니다.")
     private String name;
+
+    @Min(value = 0, message = "가격은 0원 이상이어야 합니다.")
     private int price;
+
+    @Size(min = 5, message = "설명은 5자 이상 입력해야 합니다.")
     private String description;
+
+    @Min(value = 0, message = "재고는 0개 이상이어야 합니다.")
     private int stock;
 }
